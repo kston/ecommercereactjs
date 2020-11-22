6a79ddf688926923e32d64e12505a34af6c2b35c
kston <kenestonsc@gmail.com>
2020-11-22 18:36:06 -0300

    using reselect lib c126

--- src/components/cart-dropdown/cart-dropdown.jsx
+++ src/components/cart-dropdown/cart-dropdown.jsx
@@ -5,6 +5,8 @@ import CustomButton from './../custom-button/custom-button.component';
 
 import CartItem from '../cart-item/cart-item.component';
 
+import {selectCartItems} from './../../redux/cart/cart.selector';
+
 import './cart-dropdown.styles.scss';
 
 const CartDropdown = ({cartItems}) => (
@@ -25,8 +27,8 @@ const CartDropdown = ({cartItems}) => (
     </div>
 )
 
-const mapStateToProps = ({cart: {cartItems}}) => ({
-    cartItems
+const mapStateToProps = (state) => ({
+    cartItems:selectCartItems(state)
 })
 
 export default connect(mapStateToProps)(CartDropdown);

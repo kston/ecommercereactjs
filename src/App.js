5e89105c048f351e8ec79d57f55533d2f2187258
kston <kenestonsc@gmail.com>
2020-11-22 21:57:30 -0300

    checkout page c130

--- src/App.js
+++ src/App.js
@@ -12,6 +12,7 @@ import HomePage from  './pages/homepage/homepage.component';
 import ShopPage from './pages/shop/shop.component';
 import Contact from './pages/contactpage/contactpage';
 import SignInAndSignUpPage from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component';
+import CheckoutPage from './pages/checkout/checkout.component';
 
 import { auth, createUserProfileDocument} from './firebase/firebase.utils';
 
@@ -64,6 +65,7 @@ class App extends React.Component {
                 <SignInAndSignUpPage />
               )
             }/>
+            <Route exact path='/checkout' component={CheckoutPage}/>
         </Switch>
       </div>
     
--- src/redux/cart/cart.selector.js
+++ src/redux/cart/cart.selector.js
@@ -5,3 +5,5 @@ const selectCart = state => state.cart;
 export const selectCartItems = createSelector([selectCart], (cart) => cart.cartItems);
 
 export const selectCartItemsCount = createSelector([selectCartItems], cartItems => cartItems.reduce(( accumulatedQuantity, cartItem) => accumulatedQuantity + cartItem.quantity, 0))
+
+export const selectCartTotal = createSelector([selectCartItems], cartItems => cartItems.reduce((accumulatedQuantity, cartItem)=> accumulatedQuantity + cartItem.quantity * cartItem.price, 0 ))
